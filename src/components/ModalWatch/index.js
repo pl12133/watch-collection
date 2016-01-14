c105211f212d5f4bf1f04e8605044436e7bfc3cb
pl12133 <krirken@gmail.com>
2016-01-14 17:44:24 -0500

    Fix maxHeight sizing

--- src/components/ModalWatch/index.js
+++ src/components/ModalWatch/index.js
@@ -2,11 +2,10 @@
 import React, { Component, PropTypes } from 'react';
 /* eslint-enable no-unused-vars*/
 
+import imgMaxHeight from './styles/';
 import { Modal } from 'react-overlays';
 
-import { Button, Col, Thumbnail, Image } from 'react-bootstrap';
-
-let rand = ()=> (Math.floor(Math.random() * 20) - 10);
+import { Col, Image } from 'react-bootstrap';
 
 const modalStyle = {
   position: 'fixed',
@@ -21,11 +20,9 @@ const backdropStyle = {
   opacity: 0.5
 };
 
-const dialogStyle = function() {
+const dialogStyle = function () {
   // we use some psuedo random coords so modals
   // don't sit right on top of each other.
-  let top = 50 + rand();
-  let left = 50 + rand();
 
   return {
     position: 'absolute',
@@ -34,27 +31,31 @@ const dialogStyle = function() {
     transform: 'translate(-50%, -50%)',
     border: '1px solid #e5e5e5',
     maxWidth: '400px',
+    maxHeight: '75%',
     backgroundColor: 'white',
     boxShadow: '0 5px 15px rgba(0,0,0,.5)',
     padding: 20
   };
 };
-////
+// //
 const WatchThumbnail = ({srcUrl, name, desc, onClick}) => (
-  <Thumbnail src={srcUrl} alt={name} onClick={onClick}>
-    <h3>{name}</h3>
-    <p>{desc}</p>
-  </Thumbnail>
-)
+  <div className={'thumbnail'}>
+    <Image style={ { maxHeight: '300px' } } src={srcUrl} alt={name} onClick={onClick} />
+    <div className={'caption'}>
+      <h3>{name}</h3>
+      <p>{desc}</p>
+    </div>
+  </div>
+);
 WatchThumbnail.propTypes = {
   srcUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired
-}
-////
+};
+// //
 class ModalWatch extends Component {
-  constructor(...args) {
+  constructor (...args) {
     super(...args);
     let ownFuncs = [ 'close', 'open' ];
     ownFuncs.forEach(key => (this[key])
@@ -64,11 +65,11 @@ class ModalWatch extends Component {
 
     this.state = {
       showModal: false
-    }
+    };
   }
 
-  render() {
-    let { srcUrl, name } = this.props;
+  render () {
+    let { srcUrl } = this.props;
     return (
       <Col xs={12} md={4}>
         <WatchThumbnail {...this.props} onClick={this.open}/>
@@ -85,11 +86,11 @@ class ModalWatch extends Component {
     );
   }
 
-  close(){
+  close () {
     this.setState({ showModal: false });
   }
 
-  open(){
+  open () {
     this.setState({ showModal: true });
   }
 }
@@ -98,5 +99,5 @@ ModalWatch.propTypes = {
   srcUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired
-}
+};
 export default ModalWatch;
