516614ff489694a8b1dd72211fb00564b0495374
pl12133 <krirken@gmail.com>
2016-01-14 16:54:07 -0500

    Add description to `<Watch>`

--- src/components/Watch/index.js
+++ src/components/Watch/index.js
@@ -4,16 +4,17 @@ import React, { Component, PropTypes } from 'react';
 
 import { Col, Thumbnail } from 'react-bootstrap';
 
-const Watch = ({srcUrl, alt, name}) => (
+const Watch = ({srcUrl, alt, name, desc}) => (
   <Col xs={12} md={4}>
-    <Thumbnail src={srcUrl} alt={alt || ''}>
+    <Thumbnail src={srcUrl} alt={name}>
       <h3>{name}</h3>
-      <p>{'Description'}</p>
+      <p>{desc}</p>
     </Thumbnail>
   </Col>
 );
 Watch.propTypes = {
   srcUrl: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
 };
 export default Watch;
