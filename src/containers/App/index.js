0afa6fd7955c43ef8d0513d212a8f90c966c6708
pl12133 <krirken@gmail.com>
2016-01-14 16:51:26 -0500

    Fix <NavigationHeader>

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars*/
 import React, { Component } from 'react';
 /* eslint-enable no-unused-vars*/
-import { Link } from 'react-router';
 
 import { Panel } from 'react-bootstrap';
-import { AutoAffix } from 'react-overlays';
 
 import NavigationHeader from 'components/NavigationHeader/';
 
@@ -17,13 +15,7 @@ class App extends Component {
     let { children } = this.props;
     return (
       <div>
-        <AutoAffix viewportOffsetTop={0} container={document.body}>
-          <NavigationHeader>
-            <li><Link to={'/'}>{'Home'}</Link></li>
-            <li><Link to={'/about'}>{'About'}</Link></li>
-            <li><Link to={'/watches'}>{'Watches'}</Link></li>
-          </NavigationHeader>
-        </AutoAffix>
+        <NavigationHeader brand={'Hoffman Timepieces'} fixedTop/>
         <Panel style={ { width: '90%', margin: '0 auto' } }>
           {children}
         </Panel>
